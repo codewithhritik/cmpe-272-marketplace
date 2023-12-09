ea8ecd2e83dd65c90794cfe1666d576708aeb022
Parth joshi <parthjoshi@Parths-MacBook-Air.local>
2023-12-08 21:08:39 -0800

    Removed git from playpal

--- main.php
+++ main.php
@@ -28,6 +28,11 @@
         <a href="creativehaven/index.php" class="px-6 py-2 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-600">
             <button>Visit Creative Haven</button>
         </a>
+
+
+        <a class="px-6 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+            <button>TOP 5</button>
+        </a>
     </div>
 
 </div>
